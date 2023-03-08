b3eaf4268bb735a9b955950ad3f1a691b53bfd79
Yiğit Tanyel <tanyelyigit@gmail.com>
2023-03-08 10:41:56 +0300

    initial create

---
+++ src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProductManagement.Categories;
+using ProductManagement.Products;
+
+namespace ProductManagement;
+
+public class ProductManagementApplicationAutoMapperProfile : Profile
+{
+    public ProductManagementApplicationAutoMapperProfile()
+    {
+        CreateMap<Product, ProductDto>();
+        CreateMap<CreateUpdateProductDto, Product>();
+        CreateMap<Category, CategoryLookupDto>();
+    }
+}
---
+++ src/ProductManagement.Web/ProductManagementWebAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProductManagement.Products;
+using static ProductManagement.Web.Pages.Products.CreateProductModalModel;
+
+namespace ProductManagement.Web;
+
+public class ProductManagementWebAutoMapperProfile : Profile
+{
+    public ProductManagementWebAutoMapperProfile()
+    {
+        CreateMap<CreateEditProductViewModel,CreateUpdateProductDto>();
+        CreateMap<ProductDto, CreateEditProductViewModel>();
+    }
+}
